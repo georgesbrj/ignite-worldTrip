80d186b6d23b878aae11fac171508934d5301480
George Bezerra <george.bezerra@fgv.br>
2022-08-08 16:44:39 -0300

    criando componente banner,Header e seus estilos

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+export function Banner(){
+    return(
+     <section>
+        <div>
+          <img src="/images/BannerSection.svg" alt="" />
+        </div>
+     </section>
+    );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Banner } from "../components/Banner/Banner";
+import { Header } from "../components/Header/Header";
+
+ 
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Banner />
+     </>
+  )
+}
